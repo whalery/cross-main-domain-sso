5954283fcf6289a2dfa47e03ef57b0ee269f64a1
王志坚 <32448732@qq.com>
2017-12-28 10:15:04 +0800

    跨一级域sso登录demo php p3p协议

---
+++ www.bsite.com/p3p.php
@@ -0,0 +1,33 @@
+<?php
+    header('P3P: CP="CURa ADMa DEVa PSAo PSDo OUR BUS UNI PUR INT DEM STA PRE COM NAV OTC NOI DSP COR"');
+    if (isset($_GET['sessname']) && isset($_GET['sessid'])) {
+        // cross domain request from www.bsite.com
+        setcookie($_GET['sessname'], $_GET['sessid'], time() + 3600, '/', 'www.bsite.com');
+        header("location:index.php");
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>asite cross domain set bsite cookie</title>
+    <?php
+        //在这里我没有做session redis共享的一些初始化设置，因为我不并不去读取当前session的数据内容
+        //我只需把session name 和 session id 跨域请求并传递给asite，其会根据我传递的参数在其域下设定相同的session cookie
+        session_start();
+        echo "<script src='http://www.asite.com/p3p.php?sessid=" . session_id() . "&sessname=". session_name() ."'></script>";
+    ?>
+</head>
+<body>
+    <div style="text-align: center">
+        <p>success login</p>
+    </div>
+</body>
+<script type="text/javascript">
+    window.onload = function() {
+        setTimeout(function(){
+            window.location.replace('index.php');
+        }, 1000);
+    }
+</script>
+</html>
